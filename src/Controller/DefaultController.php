9bc5da66ccda928c13898ed6c7f364eb3a0d7f7f
Arthur Haton <ah@arthurhaton.fr>
2019-03-01 23:13:19 +0100

    [Dictionnary] Create dictionnary service

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,32 +2,15 @@
 
 namespace App\Controller;
 
+use App\Service\DictionnaryService;
+use Aws\S3\S3Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index()
+    public function index(DictionnaryService $dictionnaryService)
     {
-        $datetime = new \DateTime();
-        $currenttime = $datetime->format('Gis');
-
-        if ($datetime->format('w') === 0 or $datetime->format('w') === 6) {
-            $reponse = "Mais c'est le weekend";
-        } else {
-            $reponse = "Tu peux partir !";
-
-            if ($currenttime < 80000) {
-                $reponse = "Elle a pas encore commencée !";
-            } else if ($currenttime < 160000) {
-                $reponse = "Non.";
-            } else if ($currenttime < 180000) {
-                $reponse = "Bientôt ...";
-            } else if ($currenttime > 200000) {
-                $reponse = "Toujours pas partit ???";
-            }
-        }
-
+        $reponse = $dictionnaryService->getWord();
 
         return $this->render('index.html.twig', [
             'reponse' => $reponse
