34befe28e9a2595b216870368abe30a947e52530
golpilolz <arthur.haton@orange.fr>
2019-03-31 15:46:30 +0200

    [Composer] Add logger

--- src/Service/DictionnaryService.php
+++ src/Service/DictionnaryService.php
@@ -2,13 +2,20 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+
 class DictionnaryService
 {
     private $json;
 
-    public function __construct()
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
     {
         $this->json = json_decode($this->loadDictionary());
+
+        $this->logger = $logger;
     }
 
     public function getWord(): array
@@ -17,15 +24,27 @@ class DictionnaryService
         $datetime->setTimezone(new \DateTimeZone('Europe/Paris'));
         $currenttime = $datetime->format('Gis');
 
+        if ($datetime->format('n') === "4" and $datetime->format('j') === "1") {
+
+            try {
+                if(random_int(0,1)) {
+                    return [
+                        'response' => "Oui",
+                        'gif' => "YtWWzfyXXeI5W"
+                    ];
+                }
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
+        }
+
         $words = $this->getWords((int) $currenttime);
         $key = array_rand($words);
-
-        $word = [
+        
+        return [
             'response' => $words[$key]->response,
             'gif' => $words[$key]->gif
         ];
-
-        return $word;
     }
 
     private function getWords(int $currenttime): array {
