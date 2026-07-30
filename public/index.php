10111cd8e7ffdbbab11b36ba2faef6ecd636b99c
Arthur Haton <ahaton@clever-age.com>
2026-07-30 14:49:58 +0200

    deploy(CI): change envars load

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'] ?? $_ENV['APP_ENV'], (bool) ($context['APP_DEBUG'] ?? $_ENV['APP_DEBUG']));
+    return new Kernel($_SERVER['APP_ENV'], (bool) ($_SERVER['APP_DEBUG']));
 };
